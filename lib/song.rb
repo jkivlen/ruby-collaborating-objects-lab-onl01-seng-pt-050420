43c8d318128171de395a0afd2cbade8c9e4caea5
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-06-10 17:47:54 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+  
+  attr_accessor :name, :artist
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+    
+    def self.new_by_filename(filename)
+      song_name = filename.split(" - ")[1]
+      song = Song.new(song_name)
+      artist_object = Artist.find_or_create_by_name(filename.split(" - ")[0])
+      song.artist = artist_object
+      song.artist.add_song(self)
+      song
+    end
+      
+
+  
+end
